7a2554f34005ec10bcea8f2c2857485b9985453f
yeenathan <yeenathan21@gmail.com>
2024-04-05 18:41:49 -0700

    changed backgroundColor to background for gradient

--- components/ColourBlock/index.js
+++ components/ColourBlock/index.js
@@ -3,7 +3,7 @@ import styles from "./ColourBlock.module.css";
 export default function ColourBlock({colour}) {
     return(
         <div className={styles.block}>
-            <div className={styles.colourSquare} style={{backgroundColor: `var(${colour.variable}`}}/>
+            <div className={styles.colourSquare} style={{background: `var(${colour.variable}`}}/>
             <p className="caption1">{colour.name}</p>
         </div>
     )
