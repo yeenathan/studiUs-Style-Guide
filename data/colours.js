6edee8e22c8f649cd621dd1525b349760bf948bf
yeenathan <yeenathan21@gmail.com>
2024-04-05 18:40:14 -0700

    colour data added

--- data/colours.js
+++ data/colours.js
@@ -3,14 +3,55 @@ export const colours = [
         category: "Green",
         data: [
             {name: "dark-green", variable: "--dark-green"},
-            {name: "dark-green-85", variable: "--dark-green-85"}
+            {name: "dark-green-85", variable: "--dark-green-85"},
+            {name: "med-green", variable: "--med-green"},
+            {name: "med-green-85", variable: "--med-green-85"},
+            {name: "med-green-75", variable: "--med-green-75"},
+            {name: "med-green-20", variable: "--med-green-20"},
+            {name: "med-green-10", variable: "--med-green-10"},
+            {name: "light-green", variable: "--light-green"},
+            {name: "light-green-85", variable: "--light-green-85"},
+            {name: "bright-green", variable: "--bright-green"}
+        ]
+    },
+    {
+        category: "Blue",
+        data: [
+            {name: "dark-blue", variable: "--dark-blue"},
+            {name: "bold-blue", variable: "--bold-blue"},
+            {name: "med-blue", variable: "--med-blue"},
+            {name: "med-blue-85", variable: "--med-blue-85"},
+            {name: "med-blue-35", variable: "--med-blue-35"},
+            {name: "light-blue", variable: "--light-blue"},
+            {name: "light-blue-85", variable: "--light-blue-85"},
+            {name: "bright-blue", variable: "--bright-blue"},
+            {name: "blue-bg-gradient", variable: "--blue-bg-gradient"}
         ]
     },
     {
         category: "Red",
         data: [
             {name: "dark-red", variable: "--dark-red"},
-            {name: "bold-red", variable: "--bold-red"}
+            {name: "bold-red", variable: "--bold-red"},
+            {name: "med-red", variable: "--med-red"},
+            {name: "light-red", variable: "--light-red"},
+            {name: "bright-red", variable: "--bright-red"}
+        ]
+    },
+    {
+        category: "Black",
+        data: [
+            {name: "black-text", variable: "--black-text"},
+            {name: "black-sub", variable: "--black-sub"}
+        ]
+    },
+    {
+        category: "White",
+        data: [
+            {name: "white-bg", variable: "--white-bg"},
+            {name: "white-bg-85", variable: "--white-bg-85"},
+            {name: "white-shaded", variable: "--white-shaded"},
+            {name: "white-shaded-85", variable: "--white-shaded-85"}
         ]
     }
 ]
