5e9b9cf5a9cf11ecad3366d9ac96e64a1be6f6b5
yeenathan <yeenathan21@gmail.com>
2024-04-07 21:20:01 -0700

    rearranged header links

--- components/Header/index.js
+++ components/Header/index.js
@@ -6,8 +6,8 @@ export default function Header() {
     return(
         <header className={styles.header}>
             <Link href="/" className={styles.navlink}><Image src="/images/logo/logo-colour.svg" width={594} height={260} className={styles.logo}/> Style Guide</Link>
-            <Link href="/typography" className={styles.navlink}>Typography</Link>
             <Link href="/colours" className={styles.navlink}>Colours</Link>
+            <Link href="/typography" className={styles.navlink}>Typography</Link>
             <Link href="/references" className={styles.navlink}>References</Link>
         </header>
     )
