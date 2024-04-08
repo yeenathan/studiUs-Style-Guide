5ab1cfb6eeb5fd5cb8a8799073dd2fd2fffd57f3
yeenathan <yeenathan21@gmail.com>
2024-04-07 23:37:22 -0700

    references page

--- components/Header/index.js
+++ components/Header/index.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Header() {
     return(
         <header className={styles.header}>
-            <Link href="/" className={styles.navlink}><Image src="/images/logo/logo-colour.svg" width={594} height={260} className={styles.logo}/> Style Guide</Link>
+            <Link href="/" className={styles.navlink}><Image src="/images/logo/wordmark.svg" width={594} height={260} className={styles.logo}/> Style Guide</Link>
             <Link href="/colours" className={styles.navlink}>Colours</Link>
             <Link href="/typography" className={styles.navlink}>Typography</Link>
             <Link href="/graphics" className={styles.navlink}>Graphics</Link>
--- pages/references.js
+++ pages/references.js
@@ -1,6 +1,7 @@
 import styles from "@/styles/References.module.css";
 import Header from "@/components/Header";
 import HeadArea from "@/components/HeadArea";
+import Image from "next/image";
 
 export default function References() {
     return(
@@ -10,6 +11,29 @@ export default function References() {
             <Header/>
             <div className={styles.main}>
                 <h1>References</h1>
+                <p>We took inspiration from the following layouts</p>
+                <div className={styles.references}>
+                    <div>
+                        <Image src="/images/references/pinterest.png" width={884} height={1912} style={{width: "300px", height: "auto"}}/>
+                        <p>Pinterest</p>
+                    </div>
+                    <div>
+                        <Image src="/images/references/spotify.png" width={3840} height={2160} style={{width: "900px", height: "auto"}}/>
+                        <p>Spotify</p>
+                    </div>
+                    <div>
+                        <Image src="/images/references/ubereats.png" width={862} height={1660} style={{width: "300px", height: "auto"}}/>
+                        <p>Uber Eats</p>
+                    </div>
+                    <div>
+                        <Image src="/images/references/ubereats-search.png" width={874} height={1896} style={{width: "300px", height: "auto"}}/>
+                        <p>Uber Eats</p>
+                    </div>
+                    <div>
+                        <Image src="/images/references/twitter.png" width={960} height={2080} style={{width: "300px", height: "auto"}}/>
+                        <p>Twitter/X</p>
+                    </div>
+                </div>
             </div>
         </div>
         </>
